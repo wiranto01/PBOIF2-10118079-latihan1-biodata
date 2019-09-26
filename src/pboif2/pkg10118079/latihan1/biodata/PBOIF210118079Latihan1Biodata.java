f47582f719fe39fc394291a0edf08b4b48456dc1
wiranto01 <55836685+wiranto01@users.noreply.github.com>
2019-09-26 18:15:08 +0700

    Add files via upload

---
+++ src/pboif2/pkg10118079/latihan1/biodata/PBOIF210118079Latihan1Biodata.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10118079.latihan1.biodata;
+
+/**
+ *
+ * @author
+ * Nama : Wiranto
+ * Kelas : If2
+ * Nim : 10118079
+ */
+public class PBOIF210118079Latihan1Biodata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+System.out.println("BIODATA ROCKSTAR" + "\n" + "----------------");
+        System.out.println("NAMA\t\t: WIRANTO");
+        System.out.println("ALAMAT\t\t: jln. Gelatik Dalam");
+        System.out.println("TEMPAT LAHIR\t: SUBANG");
+        System.out.println("TANGGAL LAHIR\t: 14 Agustus 1999");
+        System.out.println("--------------");
+        System.out.println("HOBI\t\t: Maen Bola");
+        System.out.println("MAKES\t\t: Nasi");
+        System.out.println("MIKES\t\t: Air");
+
+    }
+    
+}
